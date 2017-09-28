5f4c0e43dcbc9acf76b441385f83751c3f560b48
VasiliyOschepkov <vasiliyOschepkov@gmail.com>
2017-09-28 17:07:04 +0300

    Добавил Dagger 2 и RxJava

--- app/src/main/java/com/vasiliyoschepkov/flowerdelivery/view/MainActivity.java
+++ app/src/main/java/com/vasiliyoschepkov/flowerdelivery/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vasiliyoschepkov.flowerdelivery;
+package com.vasiliyoschepkov.flowerdelivery.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vasiliyoschepkov.flowerdelivery.R;
 
 public class MainActivity extends AppCompatActivity {
 
